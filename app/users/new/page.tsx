08fd1395f9ef1fe9cdf86b9f07bed04d299f3e06
anas <ktmanas2020@gmail.com>
2024-08-24 08:50:00 +0530

    users

---
+++ app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1>New page</h1>
+    </div>
+  )
+}
+
+export default NewPage
---
+++ app/users/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+
+ 
+ const UsersPage = () => {
+   return (
+     <div>
+       <div className="text-3xl font-bold underline ">
+        Users page
+        </div>
+        
+     </div>
+   )
+ }
+ 
+ export default UsersPage
+ 
